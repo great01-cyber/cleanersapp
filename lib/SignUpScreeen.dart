2b1da45c88fe5b3878279851c94ca79d76a4ed6a
Ujah Obinna John <greatujah088@gmail.com>
2025-08-20 21:45:59 +0100

    These are the files i am using for this project for trial purposes. As I work on this project, I will change some files. I face the issue of isCoreLibraryDesugaringEnabled. For me to resolve this issue I added the code snippet "isCoreLibraryDesugaringEnabled = true" in my compileOptions in the andiroid/app/gradle. I also also added depemdencies for the deusugaring to work
    Signed-off-by: Ujah Obinna John <greatujah088@gmail.com>

--- lib/SignUpScreeen.dart
+++ lib/SignUpScreeen.dart
@@ -36,87 +36,97 @@ class _SignUpScreenState extends ConsumerState<SignUpScreen> {
       appBar: AppBar(
         title: Text("Sign Up Page"),
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context)
-            .size
-            .width, // Make it take the full screen height
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              hexStringToColor("CB2B93"),
-              hexStringToColor("9546c4"),
-              hexStringToColor("5E61F4")
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+      body: loader == false
+          ? SingleChildScrollView(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context)
+                    .size
+                    .width, // Make it take the full screen height
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      hexStringToColor("CB2B93"),
+                      hexStringToColor("9546c4"),
+                      hexStringToColor("5E61F4")
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(
+                        20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 30,
+                        ),
+                        reusableTextField(
+                          "Enter Username",
+                          Icons.person_outline,
+                          false,
+                          _emailTextController,
+                          func: (value) => ref
+                              .read(signUpProvider.notifier)
+                              .onChangedUsername(value),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        reusableTextField(
+                          "Enter Email Id",
+                          Icons.lock_outline,
+                          false,
+                          _passwordTextController,
+                          func: (value) => ref
+                              .read(signUpProvider.notifier)
+                              .onChangedEmail(value),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        reusableTextField(
+                          "Enter Password",
+                          Icons.lock_outline,
+                          false,
+                          _passwordTextController,
+                          func: (value) => ref
+                              .read(signUpProvider.notifier)
+                              .onChangedPassword(value),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        reusableTextField(
+                          "Confirm password",
+                          Icons.lock_outline,
+                          false,
+                          _passwordTextController,
+                          func: (value) => ref
+                              .read(signUpProvider.notifier)
+                              .onChangedRePassword(value),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        signInSignUPButton(context, true, () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SupervisorDashboard()));
+                        }),
+                      ],
+                    ),
+                  ),
           ),
         ),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(
-                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 30,
-                ),
-                reusableTextField(
-                  "Enter Username",
-                  Icons.person_outline,
-                  false,
-                  _emailTextController,
-                  func: (value) => ref
-                      .read(signUpProvider.notifier)
-                      .onChangedUsername(value),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                reusableTextField(
-                  "Enter Email Id",
-                  Icons.lock_outline,
-                  false,
-                  _passwordTextController,
-                  func: (value) =>
-                      ref.read(signUpProvider.notifier).onChangedEmail(value),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                reusableTextField(
-                  "Enter Password",
-                  Icons.lock_outline,
-                  false,
-                  _passwordTextController,
-                  func: (value) => ref
-                      .read(signUpProvider.notifier)
-                      .onChangedPassword(value),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                reusableTextField(
-                  "Confirm password",
-                  Icons.lock_outline,
-                  false,
-                  _passwordTextController,
-                  func: (value) => ref
-                      .read(signUpProvider.notifier)
-                      .onChangedRePassword(value),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                signInSignUPButton(context, true, () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SupervisorDashboard()));
-                }),
-              ],
-            ),
-          ),
-        ),
-      ),
+            )
+          : const Center(
+              child: CircularProgressIndicator(
+              backgroundColor: Colors.blue,
+            )),
     );
   }
 }
