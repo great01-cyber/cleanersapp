7b1e9423b5e77fb2f717df01bf207ec5093547bb
Ujah Obinna John <greatujah088@gmail.com>
2025-08-20 23:38:48 +0100

    These are the files i am using for this project for trial purposes. As I work on this project, I will change some files. I face the issue of isCoreLibraryDesugaringEnabled. For me to resolve this issue I added the code snippet "isCoreLibraryDesugaringEnabled = true" in my compileOptions in the andiroid/app/gradle. I also also added depemdencies for the deusugaring to work
    Signed-off-by: Ujah Obinna John <greatujah088@gmail.com>

--- lib/Services/resuableTextField.dart
+++ lib/Services/resuableTextField.dart
@@ -45,30 +45,37 @@ TextField reusableTextField(
   );
 }
 
-Container signInSignUPButton(BuildContext context, bool isLogin, Function onTap) {
+Container signInSignUPButton(BuildContext context, bool isLogin,
+    {VoidCallback? func}) {
+  // optional void function
   return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 50,
-      margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
-      child: ElevatedButton(
-          onPressed: () {
-            onTap();
-          }, child: Text(
+    width: MediaQuery.of(context).size.width,
+    height: 50,
+    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+    child: ElevatedButton(
+      onPressed: func, // call the optional function when pressed
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith((states) {
+          if (states.contains(MaterialState.pressed)) {
+            return Colors.black26;
+          }
+          return Colors.white;
+        }),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+      ),
+      child: Text(
         isLogin ? "LOG IN" : "SIGN UP",
         style: const TextStyle(
-            color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 16),
+          color: Colors.black87,
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+        ),
       ),
-          style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.resolveWith((states) {
-                if (states.contains(MaterialState.pressed)) {
-                  return Colors.black26;
-                }
-                return Colors.white;
-              }),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))))
-      )
+    ),
   );
-
 }
